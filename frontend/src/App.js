7e518ceafdbce8087fcc71d110413726143a2841
Natalia Teaca <lilikoy@sbcglobal.net>
2020-10-22 12:24:16 -0700

    React set up

---
+++ frontend/src/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import data from './data';
+import './App.css';
+
+function App() {
+  const openMenu = () => {
+  document.querySelector(".sidebar").classList.add("open");
+}
+const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+}
+  return (
+    <div className="grid-container">
+    <header className="header">
+        <div className="brand">
+            <button onClick={openMenu}>
+                &#9776;
+            </button>
+            <a href="index.html">Michelle's Crafts</a>
+        </div>
+        <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+        </div>
+    </header>
+    <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>x
+        </button>
+        <ul>
+            <li>
+                <a href="index.html">Candles</a>
+            </li>
+            <li>
+                <a href="index.html">Soap</a>
+            </li>
+            <li>
+                <a href="index.html">Supplies</a>
+            </li>
+        </ul>
+    </aside>
+    <main className="main">
+        <div className="content">
+            <ul className="products">
+                 {/* product candle  */}
+                {
+                  data.products.map(product => 
+                    <li>
+                    <div className="product">
+                        <img className="product-image" src={ product.image } alt="product" />
+                        <div className="product-name">
+                            <a href="product.html">
+                                {product.name}
+                            </a>
+                        </div>
+                        <div className="product-brand">{product.brand}</div>
+                        <div className="product-price">${product.price}</div>
+                        <div className="product-rating">{product.rating}Stars ({product.numReviews})</div>
+                 </div>   
+                </li>)
+}           
+            </ul>
+        </div>
+    </main>
+
+    <footer className="footer">@2020 N. Teaca</footer>
+</div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,34 @@
+export default {
+    products: [
+        {
+        name: 'Candle',
+        category: 'Candles',
+        image: '/images/snowmancandle.jpg',
+        price: 30,
+        brand: 'lorem ipsum',
+        rating: 4.5,
+        numReviews: 10
+
+    },
+    {
+    name: 'Donut Soap',
+    category: 'Soap',
+    image: '/images/donutsoap.jpg',
+    price: 25,
+    brand: 'lorem ipsum',
+    rating: 4.2,
+    numReviews: 7
+
+},
+{
+    name: 'Honeycomb Mold',
+    category: 'Supplies',
+    image: '/images/honeymold.jpg',
+    price: 40,
+    brand: 'lorem ipsum',
+    rating: 5,
+    numReviews: 100
+
+},
+]
+}
