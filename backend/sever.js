182de43c1701f60b0c0fa587e35c247febcae92a
Natalia Teaca <lilikoy@sbcglobal.net>
2020-10-23 22:05:18 -0700

    node error

---
+++ backend/sever.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import data from './data';
+
+const app = express();
+
+
+app.get("/api/products", (req, res) => {
+    res.send(data.prpducts);
+})
+
+app.listen(3000, () => {console.log("Server started at http://localhost:3000")})
