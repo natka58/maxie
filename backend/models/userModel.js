b3ef68a6e74ecbe61fff1c6f1b7d03c82594e60b
Natalia Teaca <lilikoy@sbcglobal.net>
2020-10-27 12:59:26 -0700

    fixed save issue

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,57 +1,68 @@
+// import mongoose from 'mongoose';
+// // import bcrypt  from 'bcrypt';
+
+
+
+
+// const userSchema = new mongoose.Schema({
+//     name: 
+//     { 
+//         type: String, 
+//         required: true
+//     },
+//     email: 
+//     {
+//         type: String,
+//         required: true,
+//         unique:true,
+//         index:true,
+//         dropDups: true,
+//         sparse:true,
+//         match: [/.+@.+\..+/, 'Must use a valid email address'],
+//     },
+//     password: 
+//     {
+//         type: String,
+//         required: true,
+//         minlength: 5
+//     },
+//     isAdmin: 
+//     { 
+//       type: Boolean, 
+//       required: true, 
+//       default: false 
+//     },
+// });
+
+// // hash user password
+// // userSchema.pre('save', async function (next) {
+// //     if (this.isNew || this.isModified('password')) {
+// //       const saltRounds = 10;
+// //       this.password = await bcrypt.hash(this.password, saltRounds);
+// //     }
+  
+// //     next();
+// //   });
+
+// //   // custom method to compare and validate password for logging in
+// // userSchema.methods.isCorrectPassword = async function (password) {
+// //     return bcrypt.compare(password, this.password);
+// //   };
+
+// const userModel = mongoose.model('User', userSchema);
+
+// export default userModel;
+
 import mongoose from 'mongoose';
-import bcrypt  from 'bcrypt';
-
-
-// const mongoose = require('mongoose');
-
-// const { Schema } = mongoose;
-// const bcrypt = require('bcrypt');
-// const Order = require('./Order');
 
 const userSchema = new mongoose.Schema({
-    name: 
-    { 
-        type: String, 
-        required: true
-    },
-    email: 
-    {
-        type: String,
-        required: true,
-        unique:true,
-        index:true,
-        dropDups: true,
-        sparse:true,
-        match: [/.+@.+\..+/, 'Must use a valid email address'],
-    },
-    password: 
-    {
-        type: String,
-        required: true,
-        minlength: 5
-    },
-    isAdmin: 
-    { 
-      type: Boolean, 
-      required: true, 
-      default: false 
-    },
+  name: { type: String, required: true },
+  email: {
+    type: String, required: true, unique: true, index: true, dropDups: true,
+  },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, required: true, default: false },
 });
-
-// hash user password
-userSchema.pre('save', async function (next) {
-    if (this.isNew || this.isModified('password')) {
-      const saltRounds = 10;
-      this.password = await bcrypt.hash(this.password, saltRounds);
-    }
-  
-    next();
-  });
-
-  // custom method to compare and validate password for logging in
-userSchema.methods.isCorrectPassword = async function (password) {
-    return bcrypt.compare(password, this.password);
-  };
 
 const userModel = mongoose.model('User', userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,20 @@ import config from './config';
 import mongoose from 'mongoose';
 import userRoute from './routes/userRoute';
 import bodyParser from 'body-parser';
-const app = express();
+
 
 dotenv.config();
 
 const mongodbUrl = config.MONGODB_URL;
-mongoose.connect(mongodbUrl, {
-   useNewUrlParser: true,
-   useUnifiedTopology: true, 
-   useCreateIndex: true,
-}).catch(error => cpnsole.log(error.reason));
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .catch((error) => console.log(error.reason));
 
+  const app = express();
 app.use(bodyParser.json());
 app.use('/api/users', userRoute);
 
