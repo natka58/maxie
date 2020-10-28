dfc7090c07501e650ed634dfc162d851e2c33ec4
Natalia Teaca <lilikoy@sbcglobal.net>
2020-10-28 00:31:40 -0700

    delete product

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import { isAuth, isAdmin } from '../utils';
 import Product from '../models/productModel';
 
 
@@ -10,7 +10,7 @@ const products = await Product.find({});
 res.send(products);
 });
 
-router.post("/", async (req, res) =>{
+router.post("/", isAuth, isAdmin, async (req, res) =>{
     const product = new Product({
         name: req.body.name,
         price: req.body.price,
@@ -32,7 +32,7 @@ router.post("/", async (req, res) =>{
     });
 
 
-    router.put('/:id', async (req, res) => {
+    router.put('/:id', isAuth, isAdmin, async (req, res) => {
       const productId = req.params.id;
       const product = await Product.findById(productId);
       if (product) {
@@ -53,7 +53,7 @@ router.post("/", async (req, res) =>{
       return res.status(500).send({ message: ' Error in Updating Product.' });
     });
 
-    router.delete('/:id',  async (req, res) => {
+    router.delete('/:id', isAuth, isAdmin, async (req, res) => {
       const deletedProduct = await Product.findById(req.params.id);
       if (deletedProduct) {
         await deletedProduct.remove();
